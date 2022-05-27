818ca60b88bd1a72a2804f88ca09c9f0ae088608
Natasha <natalia.karpuk90@gmail.com>
2022-05-27 21:50:11 +0200

    first homework

--- js udemy/script.js
+++ js udemy/script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', '');
+
+const peronalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+}
+
+
+const answer = prompt('Один из последних просмотренных фильмов?', ''),
+    count = prompt('На сколько его оцените?', ''),
+    answer2 = prompt('Один из последних просмотренных фильмов?', ''),
+    count2 = prompt('На сколько его оцените?', '');
+
+peronalMovieDB.movies[answer] = count;
+peronalMovieDB.movies[answer2] = count2;
+
+console.log(peronalMovieDB);
